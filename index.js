53e1b3a6706003555a6af2d2aa127bf6294de0d9
TemesgenD1012 <temesgendebebe1921@gmail.com>
2023-06-16 23:26:35 -0900

    to the main

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-//  commit
+//  commit to the main 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
